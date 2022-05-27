380e6ecb79d5ce206eeca30a76ce1a12a89422e5
Jael Armas M <jaelarmas.m@gmail.com>
2022-05-26 22:29:46 -0500

    Install vector icons

--- src/components/CitiesDropDown.tsx
+++ src/components/CitiesDropDown.tsx
@@ -8,22 +8,12 @@ import {
 } from 'react-native';
 import { useAppContext } from '../context/AppContext';
 import cities from '../lib/cities.json';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const CitiesDropDown: FC<{}> = () => {
   const { selectedCity, setCity } = useAppContext();
 
   const [showOptions, setShowOptions] = useState<boolean>(false);
-
-  const Triangle: FC<{}> = () => {
-    return (
-      <View
-        style={[
-          styles.triangle,
-          !showOptions && { transform: [{ rotate: '180deg' }] },
-        ]}
-      />
-    );
-  };
 
   return (
     <View style={styles.dropDownContainer}>
@@ -31,7 +21,11 @@ const CitiesDropDown: FC<{}> = () => {
         <TouchableWithoutFeedback onPress={() => setShowOptions(!showOptions)}>
           <View style={styles.selected}>
             <Text>{selectedCity.name}</Text>
-            <Triangle />
+            <Icon
+              name={showOptions ? 'caretup' : 'caretdown'}
+              size={13}
+              color="#6B6B6B"
+            />
           </View>
         </TouchableWithoutFeedback>
       </View>
@@ -110,19 +104,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#FFFFFF',
     borderRadius: 6,
-  },
-  triangle: {
-    width: 0,
-    height: 0,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderLeftWidth: 5,
-    borderRightWidth: 5,
-    borderBottomWidth: 10,
-    borderTopColor: 'transparent',
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderBottomColor: '#6B6B6B',
   },
 });
 
